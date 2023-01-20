09806140125dc283f9c0061f2192639ad3389322
Faruk Ajiya <Ajiyafaruk@gmail.com>
2023-01-19 21:40:44 -0800

    connection

--- config/connection.js
+++ config/connection.js
@@ -11,5 +11,20 @@ const sequelize = new Sequelize(
     port: 3306,
   }
 );
-
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(
+    process.env.JAWSDB_URL
+  )
+} else {
+  const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: '127.0.0.1',
+      dialect: 'mysql',
+      port: 3306,
+    }
+    )
+}
 module.exports = sequelize;
